b67793089d674fb7f6c90060c3962549bcf1e89a
Grayson Erhard <web@graysonerhard.com>
2019-05-07 14:19:31 -0600

    intial commit

---
+++ wp-data-syndicator.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: WP Data Syndicator
+ * Version:     1.0.0
+ * Description: Syndicates all post data, custom ACF fields, and Yoast data across multiple, authenticated sites.
+ * Author:      Copper Leaf Creative
+ * Author URI:  https://copperleafcreative.com
+ * Text Domain: wp-data-syndicator
+ * Domain Path: /languages/
+ * License:     GPL v3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if ( ! function_exists( 'add_filter' ) ) {
+  header( 'Status: 403 Forbidden' );
+  header( 'HTTP/1.1 403 Forbidden' );
+  exit();
+}
+
+if ( ! defined( 'WP_DATA_SYNDICATOR_FILE' ) ) {
+  define( 'WP_DATA_SYNDICATOR_FILE', __FILE__ );
+}
+
+// Load the plugin files.
+require_once dirname( WPSEO_FILE ) . '/Includes/autoload.php';
+require_once dirname( WPSEO_FILE ) . '/Includes/enqueue.php';
