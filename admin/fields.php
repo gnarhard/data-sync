0ba44e9fe1779ee357cee67dcabb7d0ca62e2fdd
Grayson Erhard <web@graysonerhard.com>
2019-05-08 17:41:28 -0600

    added new settings fields. Not complete and hardly any of them work

--- admin/fields.php
+++ admin/fields.php
@@ -10,14 +10,51 @@ function display_source_input() {
 	       value="1" <?php checked( '1', get_option( 'source_site' ) ); ?>/> Source
 	<br>
 	<input type="radio" name="source_site" id="source_site" value="0" <?php checked( '0', get_option( 'source_site' ) );
-//	if ( $source === false ) {
-//		echo 'checked';
-//	}
+	if ( $source === false ) {
+		echo 'checked';
+	}
 	?>
 	/> Receiver
 
 	<?php
 }
+
+
+
+
+function display_push_template_button() {
+	?><button id="push_template">Push</button><?php
+}
+
+
+
+function display_push_enabled_post_types() {
+	$registered_post_types = get_post_types();
+
+	?><select name="push_enabled_post_types[]" multiple style="width: 200px; height: 300px"><?php
+
+	foreach ($registered_post_types as $key => $post_type) {
+		?><option value="<?php echo $key?>" ><?php echo $post_type?></option><?php
+	}
+
+	?></select><?php
+
+	var_dump(get_option('push_enabled_post_types'));
+}
+
+
+function display_bulk_data_push_button() {
+	?><button id="bulk_data_push">Push</button><?php
+}
+
+
+
+function display_error_log() {
+  $error_log = file_get_contents(WP_DATA_SYNDICATOR_PATH . 'error.log');
+  ?><textarea class="error_log" style="height: 500px; width: 100%;"><?php echo $error_log?></textarea><?php
+}
+
+
 
 function display_token() {
 	// UNIQUE SITE TOKEN (public key)
@@ -34,15 +71,54 @@ function display_token() {
 	}
 }
 
-/////// SOURCE OPTIONS ///////
-// Connected Site blogname, Site ID, URL, date connected
-// Synced Post Types
-// Bulk push button
-// See error log
-    // Push new template file - cpt-templates.php
+function display_notified_users() {
+
+	$users_query = new WP_User_Query( array(
+		'role' => 'administrator',
+		'orderby' => 'display_name'
+	) );  // query to get admin users
+
+	$users = $users_query->get_results();
+	$i=0;
+
+	?><select name="notified_users[]" multiple style="width: 200px;"><?php
+
+	foreach ($users as $user) {
+      ?><option value="<?php echo $user->id?>" ><?php echo $user->user_nicename?></option><?php
+  }
+
+	?></select><?php
+	var_dump(get_option('notified_users'));
+}
+
+//selected( get_option( 'notified_users' ), $user->id );
 
 
-	////// RECEIVER OPTIONS /////
-/// Users to be notified of emails
-/// Which post types to accept
-/// Pull data from source
+function display_post_types_to_accept() {
+
+	$registered_post_types = get_post_types();
+
+	?><select name="post_types_to_accept[]" multiple style="width: 200px;"><?php
+
+	foreach ($registered_post_types as $key => $post_type) {
+		?><option value="<?php echo $key?>" ><?php echo $post_type?></option><?php
+	}
+
+	?></select><?php
+
+	var_dump(get_option('post_types_to_accept'));
+
+}
+
+
+
+function display_post_types_permissions() {
+
+}
+
+
+
+
+function display_pull_data_button() {
+  ?><button id="data_pull">Pull</button><?php
+}
--- admin/register-options.php
+++ admin/register-options.php
@@ -6,10 +6,66 @@ function add_wp_data_sync_options() {
 
 	// SECTIONS
 	add_settings_section( "wp_data_sync_global_settings", "", false, 'wp-data-sync-settings' );
-	add_settings_section( "wp_data_sync_source_settings", "Source Settings", false, 'wp-data-sync-settings' );
-	add_settings_section( "wp_data_sync_receiver_settings", "Receiver Settings", false, 'wp-data-sync-settings' );
 
-	// OPTIONS
+	$source = get_option( 'source_site' );
+	if ($source === '1') {
+		add_settings_section( "wp_data_sync_source_settings", "Source Settings", false, 'wp-data-sync-settings' );
+	} else if ($source === '0') {
+		add_settings_section( "wp_data_sync_receiver_settings", "Receiver Settings", false, 'wp-data-sync-settings' );
+	}
+
+
+
+	///////////// OPTIONS /////////////////////////////
+
+	/// SOURCE OR RECEIVER
 	add_settings_field( "source_site", "Source or Receiver?", "display_source_input", 'wp-data-sync-settings', "wp_data_sync_global_settings" );
 	register_setting( "wp_data_sync_global_settings", "source_site" );
+
+
+	/////// SOURCE OPTIONS ///////
+
+	// Connected Sites
+		// blogname, Site ID, URL, date connected, remove button, connect new
+
+	// Push new template file - cpt-templates.php
+	add_settings_field( "push_template", "Push Template to Receivers", "display_push_template_button", 'wp-data-sync-settings', "wp_data_sync_source_settings" );
+
+	// Select which Post Types syndicate to which Receiver sites.
+	add_settings_field( "push_enabled_post_types", "Push-Enabled Post Types", "display_push_enabled_post_types", 'wp-data-sync-settings', "wp_data_sync_source_settings" );
+	register_setting( "wp_data_sync_receiver_settings", "display_post_types_with_push_perm" );
+
+	//	A “manual re-push” function will allow content to be selected at the Source and re-pushed to all receiving sites.  This is necessary to push out bulk uploaded / updated content.
+	add_settings_field( "bulk_data_push", "Push All Data to Receivers", "display_bulk_data_push_button", 'wp-data-sync-settings', "wp_data_sync_source_settings" );
+
+	// Error log
+	add_settings_field( "error_log", "Error Log", "display_error_log", 'wp-data-sync-settings', "wp_data_sync_source_settings" );
+
+
+
+	/////////// RECEIVER ///////////////////////////////
+
+	// SECURITY TOKEN
+//	add_settings_field( "security_token", "Security Token", "display_token", 'wp-data-sync-settings', "wp_data_sync_receiver_settings" );
+//	register_setting( "wp_data_sync_receiver_settings", "security_token" );
+
+	/// NOTIFIED USERS
+	add_settings_field( "notified_users", "Notified Users", "display_notified_users", 'wp-data-sync-settings', "wp_data_sync_receiver_settings" );
+	register_setting( "wp_data_sync_receiver_settings", "notified_users" );
+
+	/// POST TYPES TO ACCEPT
+	add_settings_field( "post_types_to_accept", "Notified Users", "display_post_types_to_accept", 'wp-data-sync-settings', "wp_data_sync_receiver_settings" );
+	register_setting( "wp_data_sync_receiver_settings", "post_types_to_accept" );
+
+	// POST TYPE PERMISSIONS
+	// Specify whether receiver site can create new posts in that CPT.  (blog, yes)
+	// Specify whether receiver site can create new taxonomy terms  (blog, yes)
+	// Editable post content, tags, custom fields
+	// ONLY able to edit status
+
+
+	// PULL DATA BUTTON
+	add_settings_field( "pull_data", "Pull All Data From Source", "display_pull_data_button", 'wp-data-sync-settings', "wp_data_sync_receiver_settings" );
+
+
 }
