ff7ce9ff4da35c1b1209de93a9cd8c04c373e31c
Grayson Erhard <web@graysonerhard.com>
2019-06-03 17:55:05 -0600

    fixed js error on receiver site caused by loading source code

--- classes/Controllers/Enqueue.php
+++ classes/Controllers/Enqueue.php
@@ -21,22 +21,27 @@ class Enqueue {
 	 * Enqueues scripts
 	 */
 	public function scripts() {
-		wp_register_script( 'data-sync-admin', DATA_SYNC_URL . 'views/dist/js/admin-autoloader.es6.js', false, 1, true );
-		wp_localize_script(
-			'data-sync-admin',
-			'DataSync',
-			array(
-				'strings' => array(
-					'saved' => __( 'Options Saved', 'text-domain' ),
-					'error' => __( 'Error', 'text-domain' ),
-				),
-				'api'     => array(
-					'url'   => esc_url_raw( rest_url( DATA_SYNC_API_BASE_URL ) ),
-					'nonce' => wp_create_nonce( 'wp_rest' ),
-				),
-			)
-		);
-		wp_enqueue_script( 'data-sync-admin' );
+
+		if ( get_option( 'source_site' ) ) {
+			wp_register_script( 'data-sync-admin', DATA_SYNC_URL . 'views/dist/js/admin-autoloader.es6.js', false, 1, true );
+			wp_localize_script(
+				'data-sync-admin',
+				'DataSync',
+				array(
+					'strings' => array(
+						'saved' => __( 'Options Saved', 'text-domain' ),
+						'error' => __( 'Error', 'text-domain' ),
+					),
+					'api'     => array(
+						'url'   => esc_url_raw( rest_url( DATA_SYNC_API_BASE_URL ) ),
+						'nonce' => wp_create_nonce( 'wp_rest' ),
+					),
+					'source_site'  => get_option( 'source_site' ),
+				)
+			);
+
+			wp_enqueue_script( 'data-sync-admin' );
+		}
 	}
 
 	/**
