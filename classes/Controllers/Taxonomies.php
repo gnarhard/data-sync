e72ab520e9c6f880ee7532aff94672fa1bf92a89
Grayson Erhard <web@graysonerhard.com>
2019-06-19 19:20:43 -0600

    fixed taxonomy error from taxonomy_data being false and going into the foreach loop

--- classes/Controllers/Taxonomies.php
+++ classes/Controllers/Taxonomies.php
@@ -8,9 +8,12 @@ class Taxonomies {
 
 	public function __construct( $post_id, $taxonomies ) {
 		foreach ( $taxonomies as $taxonomy_slug => $taxonomy_data ) {
-			foreach ( $taxonomy_data as $term ) {
-				wp_set_object_terms( $post_id, $term->slug, $taxonomy_slug );
+			if ( false !== $taxonomy_data ) {
+				foreach ( $taxonomy_data as $term ) {
+					wp_set_object_terms( $post_id, $term->slug, $taxonomy_slug );
+				}
 			}
+
 		}
 	}
 }
