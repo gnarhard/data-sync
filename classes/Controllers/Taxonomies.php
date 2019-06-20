76e23e9a19f8f0871590676dec87c67a7f24f85e
Grayson Erhard <web@graysonerhard.com>
2019-06-19 19:22:34 -0600

    added docbloc to taxonomies

--- classes/Controllers/Taxonomies.php
+++ classes/Controllers/Taxonomies.php
@@ -4,16 +4,27 @@
 namespace DataSync\Controllers;
 
 
+/**
+ * Class Taxonomies
+ * @package DataSync\Controllers
+ */
 class Taxonomies {
 
-	public function __construct( $post_id, $taxonomies ) {
+	/**
+	 * Taxonomies constructor.
+	 *
+	 * Creates/Updates all terms sent from source.
+	 *
+	 * @param int $post_id
+	 * @param object $taxonomies
+	 */
+	public function __construct( int $post_id, object $taxonomies ) {
 		foreach ( $taxonomies as $taxonomy_slug => $taxonomy_data ) {
 			if ( false !== $taxonomy_data ) {
 				foreach ( $taxonomy_data as $term ) {
 					wp_set_object_terms( $post_id, $term->slug, $taxonomy_slug );
 				}
 			}
-
 		}
 	}
 }
