7380542ed0f4143454bb4e8008114ca7f3bb33ba
Grayson Erhard <web@graysonerhard.com>
2019-06-20 16:08:08 -0600

    reversed posts to show most recent first on dashboard widget

--- views/admin/widgets/status-dashboard.php
+++ views/admin/widgets/status-dashboard.php
@@ -10,7 +10,7 @@ use DataSync\Models\SyncedPost;
  * Dashboard widget that displays status of posts that haven't been synced
  */
 function status_widget() {
-	// TODO: add pagination
+
 	?>
 	<table id="wp_data_sync_status">
 		<thead>
@@ -28,39 +28,48 @@ function status_widget() {
 		$number_of_sites_connected = count( $connected_sites );
 		$source_options            = Options::source()->get_data();
 		$posts                     = Posts::get( array_keys( $source_options->push_enabled_post_types ) );
-		foreach ( $posts as $post ) {
-			$post = $post[0];
+		$sorted_posts              = clone( (object) array_reverse( (array) $posts) );
 
-			$time = strtotime( $post->post_date );
+		foreach ( $sorted_posts as $post ) {
 
-			$result = SyncedPost::get_where(
-				array(
-					'source_post_id' => (int) filter_var( $post->ID, FILTER_SANITIZE_NUMBER_INT ),
-				)
-			);
+			if ( ! empty( $post ) ) {
+				$post = $post[0];
 
-			if ( count( $result ) === $number_of_sites_connected ) {
-				$post_status = '<i class="dashicons dashicons-yes" title="Synced on all connected sites."></i>';
-			} else if ( count( $result ) < $number_of_sites_connected ) {
-				$post_status = '<i class="dashicons dashicons-info" title="Partially synced. Some posts may have failed to sync with a connected site due to an error."></i>';
-			} else {
-				$post_status = '<i class="dashicons dashicons-warning" title="Not synced."></i>';
+				$time = strtotime( $post->post_date );
+
+				$result = SyncedPost::get_where(
+					array(
+						'source_post_id' => (int) filter_var( $post->ID, FILTER_SANITIZE_NUMBER_INT ),
+					)
+				);
+
+				if ( count( $result ) === $number_of_sites_connected ) {
+					$post_status = '<i class="dashicons dashicons-yes" title="Synced on all connected sites."></i>';
+				} else if ( count( $result ) === 0 ) {
+					$post_status = '<i class="dashicons dashicons-info" title="Partially synced. Some posts may have failed to sync with a connected site due to an error."></i>';
+				} else {
+					$post_status = '<i class="dashicons dashicons-warning" title="Not synced."></i>';
+				}
+				?>
+				<tr>
+					<td><?php echo $post->ID ?></td>
+					<td><?php echo $post->post_title ?></td>
+					<td><?php echo ucfirst( $post->post_type ); ?></td>
+					<td><?php echo date( 'g:i a - F j, Y', $time ); ?></td>
+					<td><?php echo $post_status; ?></td>
+				</tr>
+				<?php
 			}
-			?>
-			<tr>
-				<td><?php echo $post->ID ?></td>
-				<td><?php echo $post->post_title ?></td>
-				<td><?php echo ucfirst( $post->post_type ); ?></td>
-				<td><?php echo date( 'g:i a - F j, Y', $time ); ?></td>
-				<td><?php echo $post_status; ?></td>
-			</tr>
-			<?php
+
 		}
 		?>
 	</table>
 	<div id="status_dashboard_button_wrap">
 		<button id="bulk_data_push"><?php _e( 'Sync', 'data_sync' ); ?></button>
 		<button id="template_push"><?php _e( 'Push Template', 'data_sync' ); ?></button>
+	</div>
+	<div id="error_log_wrap">
+		<a href="<?php echo admin_url( 'options-general.php?page=data-sync-settings' ); ?>">Go to error log</a>
 	</div>
 	<?php
 }
